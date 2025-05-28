ffd6e44d34487a4c28f7fbc87a525ffa23f3152e
Max Reinartz <86135033+maxreinartz@users.noreply.github.com>
2025-05-27 22:17:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 7689;
 
 app.use(express.static(path.join(__dirname, 'public')));
